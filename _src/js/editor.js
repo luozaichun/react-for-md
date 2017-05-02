ebbde6363f19688152dc6065971b1e7acac39cf9
luozaichun <952467274@qq.com>
2017-05-02 18:22:34 +0800

    add constructor

--- _src/js/editor.js
+++ _src/js/editor.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import marked from 'marked';
 import style from '../css/style.css';
+import classNames from 'classnames';
 class Eidtor extends React.Component {
-    static propTypes = { // as static property
-        content: React.PropTypes.string.isRequired
-    };
+    // static propTypes = {
+    //     // content: React.PropTypes.string.isRequired
+    // };
     constructor(props){
         super(props);
         this.state = {
-            panelClass: 'md-panel',
+            panelClass: 'mod-panel',
             mode: 'split',
             isFullScreen: false,
-            content: marked(this.props.content || '')
+            theme:'light',
+            value: marked('')
         }
     }
     render() {
+        const panelClass=classNames([ 'mod-panel', 'clearfix',{ 'fullscreen': this.state.isFullScreen },{ 'dark': this.state.mode === 'dark'} ]);
+        const editorClass = classNames([ 'mod-editor', { 'main-mode': this.state.mode === 'edit' } ]);
+        const previewClass = classNames([ 'mod-preview', { 'hidden': this.state.mode === 'edit','main-mode': this.state.mode === 'preview'} ]);
+        let value = this.state.value;
         return (
-            <div className="h">
-                Hello 
+            <div className={panelClass}>
+                <div className="mod-tool">
+                    <ul className="edit-toolbar">
+                        
+                    </ul>
+                    <ul className="preview-toolbar">
+
+                    </ul>
+                </div>
+                <div className={editorClass}>
+                    <textarea name="content" value={value} onChange={this.handleChange.bind(this)}></textarea>
+                </div>
+                <div className={previewClass} dangerouslySetInnerHTML={{ __html: this.state.value }}></div>
             </div>
         );
+    }
+    handleChange (event) {
+        this.setState({
+            value: event.target.value
+        });
+        // setTimeout(() => {
+        //     this.setState({ content: marked(e.target.value) });
+        // }, 300)
     }
 }
 export default Eidtor;
