f1ecc61d8721a78c1dee5df81693a2160715fe51
luozaichun <952467274@qq.com>
2017-05-03 19:18:46 +0800

    add tool and font-awesome

--- _src/js/editor.js
+++ _src/js/editor.js
@@ -3,7 +3,7 @@ import marked from 'marked';
 import highlight from "highlight.js";
 import classNames from 'classnames';
 import style from '../css/style.css';
-import '../css/editor.css';
+import '../css/highlight.css';
 
 marked.setOptions({
     renderer: new marked.Renderer(),
@@ -30,7 +30,7 @@ class Eidtor extends React.Component {
             mode: 'split',
             isFullScreen: false,
             theme:'light',
-            content: marked('')
+            content: marked(this.props.content || '')
         }
     }
     render() {
@@ -40,15 +40,13 @@ class Eidtor extends React.Component {
         return (
             <div className={panelClass}>
                 <div className="mod-tool">
-                    <ul className="edit-toolbar">
-                        
-                    </ul>
+                    {this.toolbar()}
                     <ul className="preview-toolbar">
 
                     </ul>
                 </div>
                 <div className={editorClass}>
-                    <textarea name="content" ref="editor" onChange={this.handleChange.bind(this)}></textarea>
+                    <textarea name="content" ref="editor" onChange={()=>this.handleChange()}></textarea>
                 </div>
                 <div className={previewClass} dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
             </div>
@@ -57,6 +55,20 @@ class Eidtor extends React.Component {
     handleChange () {
         this.setState({ content: marked(this.refs.editor.value) });
     }
-
+    toolbar(){
+       return(
+           <ul className="edit-toolbar clearfix">
+             <li className="tb-btn"><a title="加粗"><i className="fa fa-bold"></i></a></li>
+             <li className="tb-btn"><a title="斜体"><i className="fa fa-italic"></i></a></li>
+             <li className="tb-btn"><a title="链接"><i className="fa fa-link"></i></a></li>
+             <li className="tb-btn"><a title="引用"><i className="fa fa-quote-left"></i></a></li>
+             <li className="tb-btn"><a title="代码段"><i className="fa fa-code"></i></a></li>
+             <li className="tb-btn"><a title="图片"><i className="fa fa-picture-o"></i></a></li>
+             <li className="tb-btn"><a title="有序列表"><i className="fa fa-list-ol"></i></a></li>
+             <li className="tb-btn"><a title="无序列表"><i className="fa fa-list-ul"></i></a></li>
+             <li className="tb-btn"><a title="标题" ><i className="fa fa-header"></i></a></li>
+           </ul>
+       )
+    }
 }
 export default Eidtor;
