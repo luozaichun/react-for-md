cf5b8743dc793df36e18c521e34a57c70097072c
luozaichun <952467274@qq.com>
2017-05-04 15:29:26 +0800

    adjust css

--- _src/js/editor.js
+++ _src/js/editor.js
@@ -2,8 +2,6 @@ import React from 'react';
 import marked from 'marked';
 import highlight from "highlight.js";
 import classNames from 'classnames';
-import style from '../css/style.css';
-import '../css/highlight.css';
 
 marked.setOptions({
     renderer: new marked.Renderer(),
@@ -36,19 +34,25 @@ class Eidtor extends React.Component {
     render() {
         const panelClass=classNames([ 'mod-panel', 'clearfix',{ 'fullscreen': this.state.isFullScreen },{ 'dark': this.state.mode === 'dark'} ]);
         const editorClass = classNames([ 'mod-editor', { 'main-mode': this.state.mode === 'edit' } ]);
-        const previewClass = classNames([ 'mod-preview','markdown-body',{ 'hidden': this.state.mode === 'edit','main-mode': this.state.mode === 'preview'} ]);
+        const previewClass = classNames([ 'mod-preview',{ 'hidden': this.state.mode === 'edit','main-mode': this.state.mode === 'preview'} ]);
         return (
             <div className={panelClass}>
-                <div className="mod-tool">
+                <div className="mod-tool clearfix">
                     {this.toolbar()}
-                    <ul className="preview-toolbar">
-
-                    </ul>
+                    {this.modebat()}
                 </div>
-                <div className={editorClass}>
-                    <textarea name="content" ref="editor" onChange={()=>this.handleChange()}></textarea>
+                <div className="m-content">
+                    <div className={editorClass}>
+                        <div className="m-editor">
+                            <textarea name="content" ref="editor" onChange={()=>this.handleChange()}></textarea>
+                        </div>
+                    </div>
+                    <div className={previewClass}>
+                        <div className="m-preview">
+                            <div className="markdown-body" dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
+                        </div>
+                    </div>
                 </div>
-                <div className={previewClass} dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
             </div>
         );
     }
@@ -58,17 +62,50 @@ class Eidtor extends React.Component {
     toolbar(){
        return(
            <ul className="edit-toolbar clearfix">
-             <li className="tb-btn"><a title="加粗"><i className="fa fa-bold"></i></a></li>
-             <li className="tb-btn"><a title="斜体"><i className="fa fa-italic"></i></a></li>
-             <li className="tb-btn"><a title="链接"><i className="fa fa-link"></i></a></li>
-             <li className="tb-btn"><a title="引用"><i className="fa fa-quote-left"></i></a></li>
-             <li className="tb-btn"><a title="代码段"><i className="fa fa-code"></i></a></li>
-             <li className="tb-btn"><a title="图片"><i className="fa fa-picture-o"></i></a></li>
-             <li className="tb-btn"><a title="有序列表"><i className="fa fa-list-ol"></i></a></li>
-             <li className="tb-btn"><a title="无序列表"><i className="fa fa-list-ul"></i></a></li>
-             <li className="tb-btn"><a title="标题" ><i className="fa fa-header"></i></a></li>
+             <li><a title="加粗"><i className="fa fa-bold"></i></a></li>
+             <li><a title="斜体"><i className="fa fa-italic"></i></a></li>
+             <li><a title="链接"><i className="fa fa-link"></i></a></li>
+             <li><a title="引用"><i className="fa fa-quote-left"></i></a></li>
+             <li><a title="代码段"><i className="fa fa-code"></i></a></li>
+             <li><a title="图片"><i className="fa fa-picture-o"></i></a></li>
+             <li><a title="有序列表"><i className="fa fa-list-ol"></i></a></li>
+             <li><a title="无序列表"><i className="fa fa-list-ul"></i></a></li>
+             <li><a title="标题" ><i className="fa fa-header"></i></a></li>
            </ul>
        )
     }
+    modebat(){
+        return(
+            <ul className="preview-toolbar clearfix">
+                <li>
+                    <a title="全屏模式" >
+                        <i className="fa fa-arrows-alt"></i>
+                    </a>
+                </li>
+                <li>
+                    <a title="主体切换">
+                        <i className="fa fa-adjust"></i>
+                    </a>
+                </li>
+                <li>
+                    <a title="阅读模式">
+                        <i className="fa fa-eye"></i>
+                    </a>
+                </li>
+                
+                <li>
+                    <a title="分屏模式">
+                        <i className="fa fa-columns"></i>
+                    </a>
+                </li>
+                <li>
+                    <a title="编辑模式">
+                        <i className="fa fa-pencil"></i>
+                    </a>
+                </li>
+            </ul>  
+        ) 
+    }
+    
 }
 export default Eidtor;
--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from '../_src/js/main';
-
+import '../_src/css/style.css';
+import '../_src/css/highlight.css';
 ReactDOM.render(
     <Main />,
     document.getElementById('app')
